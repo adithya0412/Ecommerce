50d6e29405d5d90dbcc02934358b9aea572c35c0
adithya0412 <adithyakcadithya@gmail.com>
2025-11-12 11:35:33 +0530

    hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['*', 'ecommerce-abc.netlify.app', 'ecommerce-2l31.onrender.com' ],
+    allowedHosts: ['*', 'ecommerce-abc.netlify.app', 'ecommerce-2l31.onrender.com', 'ecommerce-frontend-9t4t.onrender.com' ],
     port: 5173,
     proxy: {
       "/api": {
